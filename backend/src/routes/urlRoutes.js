babe641ac8e71e07e225bac674597a5ec4ca2d00
kartikey-singh-k <144022665+kartikey-singh-k@users.noreply.github.com>
2026-06-07 02:47:41 +0530

    changes

--- backend/src/routes/urlRoutes.js
+++ backend/src/routes/urlRoutes.js
@@ -1,16 +1,20 @@
 import express from 'express';
 import { shortenUrl, redirectUrl, getMyUrls } from '../controllers/urlController.js';
-import authMiddleware from '../middlewares/authMiddleware.js';
 import { generateQrCode } from '../controllers/qrController.js';
+import authMiddleware from '../middlewares/authMiddleware.js';
+import { slidingWindowLimiter } from '../middlewares/rateLimiter.js'; // ✅ NEW: Import your rate limiter
+
 const router = express.Router();
 
-// 1. Create Short Link (Protected)
-router.post('/shorten', authMiddleware, shortenUrl);
-
-// 2. Get My Links (Protected) <--- ADD THIS NEW ROUTE
+// 1. Create Short Link (Protected AND Rate Limited)
+router.post('/shorten', authMiddleware, slidingWindowLimiter, shortenUrl);
+// 2. Get User's Links (Protected)
 router.get('/myurls', authMiddleware, getMyUrls);
 
-// 3. Redirect (Public) - KEEP THIS AT THE BOTTOM
-router.get('/:shortCode', redirectUrl);
+// 3. Generate QR Code
 router.get('/qr/:shortCode', generateQrCode);
+
+// 4. Redirect (Public) 
+router.get('/:shortCode', redirectUrl);
+
 export default router;
